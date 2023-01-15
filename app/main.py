575ef1bb5e2de4023ecdaea3f08d1dc8750b1d2e
quentinalais <quentin.alais@hotmail.fr>
2023-01-15 00:28:46 +0000

    work in progress

--- app/main.py
+++ app/main.py
@@ -16,6 +16,12 @@ def read_root():
     return "Greeting message !"
 
 
+@app.get("/bonjour")
+def bonjour():
+    return "bonjour"
+
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
