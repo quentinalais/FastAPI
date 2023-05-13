3b9679bc45650893da9cffc86c540e184f2c9db1
quentinalais <quentin.alais@hotmail.fr>
2023-05-13 02:00:21 +0100

    add docker compose

--- app/main.py
+++ app/main.py
@@ -7,33 +7,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Hola como ca va Quentin?"}
+    return "Hello World"
 
-@app.get("/greeting")
+@app.get("/helo")
 def read_root():
-    return "Greeting message !"
-
-
-@app.get("/bonjourtoutlemonde")
-def bonjour():
-    return "bonjour a tous et a toute"
-
-@app.get("/holacomoestas")
-def bonjour():
-    return "Hola como estas?"
-
-@app.get("/new_endpoint")
-def bonjour():
-    return "Hello"
-
-@app.get("/new_endpoint4")
-def bonjour():
-    return "Hello"
-
-@app.get("/new_endpoint6")
-def bonjour():
-    return "Hello"
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return "Bonjour"
