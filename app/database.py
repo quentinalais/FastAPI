654399dd30fe2f47177a43207f783d367f44e663
quentinalais <quentin.alais@hotmail.fr>
2024-02-08 22:05:54 +0000

    add .env

--- app/database.py
+++ app/database.py
@@ -1,12 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = 'postgres'
-DB_PASSWORD = 'postgres'
-DB_PORT = '5432'
-DB_HOST = '192.168.1.200' # service
-DB_DATABASE = 'DB'
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
+DB_HOST = os.getenv("DB_HOST") # service
+DB_DATABASE = os.getenv("DB_DATABASE")
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}'
 
