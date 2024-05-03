486acb3052f609c060ed19021e4825c42f70d306
quentinalais <quentin.alais@hotmail.fr>
2024-05-03 22:02:50 +0100

    Add docker-compose.debug.yml

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi import Depends, FastAPI, HTTPException,  File, UploadFile
 from sqlalchemy.orm import Session
 from fastapi.encoders import jsonable_encoder
 
-import crud, models
+import models
 from database import SessionLocal, engine, Base
 
 from pydantic import BaseModel
@@ -15,7 +15,6 @@ app = FastAPI(title="Raspberry PI Hosted Fast API")
 
 Base.metadata.create_all(bind=engine)
 
-
 # Dependency
 def get_db():
     db = SessionLocal()
@@ -24,23 +23,17 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/actors/", )
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_actors(db, skip=skip, limit=limit)
-    return users
 
 @app.get("/tracks/",)
 def get_tracks( db: Session = Depends(get_db)):
     tracks = db.query(models.Tracks).all()
-    for track in tracks:
-        print(dict(track))
 
     return "Hello World"
 
 @app.post("/upload_track/")
 async def create_upload_file(file: UploadFile = File(...), db:Session = Depends(get_db)):
     content =  await file.read()
-    new_track = models.Tracks(ID=1, filename=file.filename, content=content)
+    new_track = models.Tracks(filename=file.filename, content=content)
     db.add(new_track)
     db.commit()
     db.refresh(new_track)
--- app/models.py
+++ app/models.py
@@ -5,14 +5,6 @@ from database import Base
 from pydantic import BaseModel
 
 
-class Actor(Base):
-    __tablename__ = "actor"
-    actor_id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    last_update = Column(DateTime)
-
-
 class Tracks(Base):
     __tablename__ = "Tracks"
     ID = Column(Integer, primary_key=True, autoincrement=True)
