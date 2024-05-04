362b222ae0c61f46aa33925d27ad10517fffa2dc
quentinalais <quentin.alais@hotmail.fr>
2024-05-04 12:24:39 +0100

    add template frontend

---
+++ frontend/src/views/IndexSections/Download.js
@@ -0,0 +1,55 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// reactstrap components
+import { Button, Container, Row, Col } from "reactstrap";
+
+export default function Download() {
+  return (
+    <div
+      className="section section-download"
+      data-background-color="black"
+      id="download-section"
+    >
+      <img alt="..." className="path" src={require("assets/img/path1.png")} />
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col className="text-center" lg="8" md="12">
+            <h2 className="title">
+              Only few seconds away from eternel glory.
+            </h2>
+            <h4 className="description">
+              Now, upload your muisc !
+            </h4>
+          </Col>
+          <Col className="text-center" lg="8" md="12">
+            <Button
+              className="btn-round"
+              color="info"
+              href="https://www.creative-tim.com/product/blk-design-system-react?ref=blkdsr-index-page-download-section"
+              role="button"
+              size="lg"
+            >
+              Upload Music
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
