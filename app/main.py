e0e2e5c58695856ab720f9cde5f9e23a13f739d4
quentinalais <quentin.alais@hotmail.fr>
2024-07-02 22:27:36 +0100

    update

--- app/main.py
+++ app/main.py
@@ -28,7 +28,8 @@ s3 = boto3.client('s3',
 app = FastAPI(title="Raspberry PI Hosted Fast API")
 
 origins = [
-    "https://raspberrypi-fastapi.at.remote.it:33000"
+    "https://raspberrypi-fastapi.at.remote.it:33000",
+    "https://raspberrypi-react.at.remote.it:33001"
 ]
 
 app.add_middleware(
